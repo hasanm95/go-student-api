e5037ba73205bafd2cdfa124bf20fac01750d099
hasanm95 <hsncool10@gmail.com>
2025-01-06 19:17:01 +0800

    config load func

---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+
+)
+
+type HTTPServer struct {
+	Addr string `yaml:"address"`
+}
+
+type Config struct {
+	Env  string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer HTTPServer `yaml:"http_server"`
+}
+
+func MustLoad() *Config{
+	// Load config from file
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is required")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file not found: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanEnv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err.Error())
+	}
+
+	return &cfg
+}
